98d16c29512159e7ae5fe69488e6f1f345f81d79
Heuron <heuron-patapon@laposte.net>
2022-12-27 16:34:55 +0100

    version 1.1.2 fix dependencies

--- src/hpat/sylfr/__init__.py
+++ src/hpat/sylfr/__init__.py
@@ -4,7 +4,7 @@ Syllabification de mots en français.
 
 __author__ = "Heuron Patapon"
 __email__ = "heuron-patapon@laposte.net"
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 import re
 import unittest
